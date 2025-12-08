6f0705edee35adeef8f64f763c2b9b1eedaa9162
SajidCyber <actorsajid132@gamil.com>
2025-12-08 12:15:38 +0530

    Event Handling

--- React/eventhandling/src/App.jsx
+++ React/eventhandling/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Content from './Content'
+import Header from './Header'
 
 function App() {
- let demo=(name)=>{
-  console.log("hai"+ name)
- }
-
-  return (
-    <>
-      <button onclick={()=>demo("BLACKYY")}>Click Me</button>
-    </>
-  )
+ return (
+  <>
+  <Header />
+  <Content />
+  </>
+ )
 }
 
 export default App
---
+++ React/eventhandling/src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+const Header = () =>
+{
+    let des={backgroundColor: "black", color:"white"};
+    return (
+        <div>
+            <h1 style={des}>Tiger</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                 Asperiores ab odio ipsa fugit saepe quod voluptates dolorem,
+                 eveniet cum itaque ea facilis eius facere corrupti adipisci autem sapiente rem ex!</p>
+        </div>
+        
+    )
+}
+
+export default Header
